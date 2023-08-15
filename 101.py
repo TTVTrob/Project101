3e76f12e18f014dd578b307dddffec966dc7a3d1
TTVTrob <82639775+TTVTrob@users.noreply.github.com>
2023-08-15 18:31:48 +0530

    Add files via upload

---
+++ 101.py
@@ -0,0 +1,53 @@
+import random
+a="y"
+while a=="y":
+    b=random.randint(1,6)
+    if b==1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+        
+    if b==2 :
+        print("[-----]")
+        print("[     ]")
+        print("[0   0]")
+        print("[     ]")
+        print("[-----]")
+        
+    if b==3:
+        print("[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[-----]")
+        
+    if b==4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+        
+    if b==5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+        
+    if b==6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+    a=input("press y to roll again and n to exit")
+    print("\n")
+        
+        
+
+   
+    
+
